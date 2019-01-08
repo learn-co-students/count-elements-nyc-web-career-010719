5f76eff8a4a22ce34ec2d9dfc0c018680e78c655
github username <github email address>
2019-01-07 19:34:43 -0500

    Solution added

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
 def count_elements(array)
+<<<<<<< HEAD
   # code goes here
   array.inject(Hash.new(0)) {|words,count| words[count] += 1 ; words}
+=======
+  count = Hash.new(0)
+  array.each {|item| count[item] += 1 }
+  count
+
+  # Fancy
+  # array.each_with_object(Hash.new(0)) do |item, count|
+  #   count[item] += 1
+  # end
+>>>>>>> a4afb18a0110ce1dbdf381e0b5970af58ee2b014
 end
