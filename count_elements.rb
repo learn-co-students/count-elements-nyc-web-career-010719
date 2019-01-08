c66b8f3e0086214510768af2edf0c2a5a63f2d40
jbharwood <josephblakeharwood@gmail.com>
2019-01-08 15:39:45 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,19 @@
+require "pry"
+
 def count_elements(array)
-  # code goes here
+  array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  # new_hash = {}
+  # count = 0
+  # array.each do |x|
+  #   if array.count(x) == 1
+  #     new_hash[x] = x
+  #     new_hash[x] = count
+  #   else
+  #     count += 1
+  #     new_hash[x] = count
+  #   end
+  # end
+  # new_hash
 end
- 
+
+# binding.pry
